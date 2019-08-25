58ba131d8f78b73eb9850e2dce5597d82d63787e
Frank Xiong <Frank_Xiong@outlook.com>
2019-08-25 17:39:23 +0800

    update for dance

--- Dance/projects/sources/main.cpp
+++ Dance/projects/sources/main.cpp
@@ -5,6 +5,8 @@
 
 #include "dance_action.h"
 
+constexpr unsigned int dance_number = 1000000000;
+
 inline void show(const char(&s)[length])
 {
     for (char x : s) std::cout << x;
@@ -21,85 +23,69 @@ inline bool is_origin(const char(&s)[length], const char* origin)
     return true;
 }
 
-void case1(const std::string& text)
+void case1(const std::string& text, char (&s)[length])
 {
-    char s[length]{ 0 };
-    for (int i = 0; i < length; ++i) s[i] = 'a' + i;
-    show(s);
-
     dance_action::action_list actions;
     unsigned int n = dance_action::parse_action(text, actions);
     std::cout << "parse action size: " << n << std::endl;
 
-    clock_t t1 = clock();
     for (auto& action : actions) action(s);
-    clock_t t2 = clock();
-
-    show(s); // cknmidebghlajpfo
-
-    std::cout << "time: " << t2 - t1 << std::endl;
 }
 
-void case2(const std::string& text)
+void case2(const std::string& text, char (&s)[length])
 {
-    char s[length]{ 0 };
-    for (int i = 0; i < length; ++i) s[i] = 'a' + i;
-    show(s);
-
     dance_map dm(text);
 
     clock_t t1 = clock();
 
-    for (int i = 0; i < 1000000000; ++i)
-        dm(s);
+    for (int i = 0; i < dance_number; ++i) dm(s);
 
     clock_t t2 = clock();
-
-    show(s); // cbolhmkgfpenidaj
 
     std::cout << "time: " << t2 - t1 << std::endl;
 }
 
-void case3(const std::string& text)
+void case3(const std::string& text, char (&s)[length])
 {
-    char s[length]{ 0 };
-    for (int i = 0; i < length; ++i) s[i] = 'a' + i;
-    show(s);
-
     dance_action::action_list actions;
     dance_action::parse_action(text, actions);
 
-    int n = 1;
-
     clock_t t1 = clock();
 
-    for (auto& action : actions) action(s);
+    int n = 1;
+    for (auto& action : actions)
+        action(s);
 
     for (; !is_origin(s, "abcdefghijklmnop"); ++n)
-        for (auto& action : actions) action(s);
+        for (auto& action : actions)
+            action(s);
 
-    for (int i = 0; i < 1000000000 % n; ++i)
-        for (auto& action : actions) action(s);
+    for (int i = 0; i < (dance_number % n); ++i)
+        for (auto& action : actions)
+            action(s);
 
     clock_t t2 = clock();
 
-    show(s); // cbolhmkgfpenidaj
-
+    std::cout << "circle number: " << n << std::endl;
     std::cout << "time: " << t2 - t1 << std::endl;
 }
 
 int main()
 {
+    char s[length]{ 0 };
+    for (int i = 0; i < length; ++i) s[i] = 'a' + i;
+    show(s);
+
     std::ifstream fin("../resources/actions.txt", std::ios::in);
     std::istreambuf_iterator<char> beg(fin), end;
     std::string action_text(beg, end);
     fin.close();
 
-    // case1(action_text);
+    void (*func[])(const std::string&, char(&)[length]) = { case1, case2, case3 };
+          
+    func[2](action_text, s);
 
-    // case2(action_text);
-
-    case3(action_text);
+    show(s);
 
     return 0;
 }
