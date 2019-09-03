e84ed5794e0d0b377cf2aa747ebfcf82165779c5
Frank Xiong <Frank_Xiong_@outlook.com>
2019-09-03 15:07:18 +0800

    update eight queens for clojure

--- EightQueens/projects/Clojure/EightQueens/src/eight_queens.clj
+++ EightQueens/projects/Clojure/EightQueens/src/eight_queens.clj
@@ -12,30 +12,28 @@
           (recur (inc i)))
         true))))
 
-(defn permutation "获取序列coll满足检查条件check?的n个元素的排列"
-  [coll n check?]
-  (let [v    (vec coll)
-        size (count v)]
-    (if (or (< n 1) (< size n))
-      []
-      (loop [result [] stack [] begin 0]
-        (if-let [x (some (fn [i] (when (check? stack i) i)) (range begin size))]
-          (let [s (conj stack x)]
-            (if (= n (count s))
-              (recur (conj result (map v s)) stack (inc x))
-              (recur result s 0)))
-          (if (empty? stack)
-            result
-            (recur result (pop stack) (inc (last stack)))))))))
+(defn permutation "获取n个元素满足检查条件check?的排列"
+  [n check?]
+  (if (< 0 n)
+    (loop [result [] stack [] begin 0]
+      (if-let [x (some (fn [i] (when (check? stack i) i)) (range begin n))]
+        (let [s (conj stack x)]
+          (if (= n (count s))
+            (recur (conj result s) stack (inc x))
+            (recur result s 0)))
+        (if (empty? stack)
+          result
+          (recur result (pop stack) (inc (last stack))))))
+    []))
 
 (defn show "打印一个盘面"
   [eq-map]
   (let [line (vec (repeat (count eq-map) \-))]
     (doseq [i eq-map]
       (apply println (assoc line i \*)))
-    (println "===================")))
+    (println "====================")))
 
 (defn -main []
-  (let [eq-maps (permutation (range 8) 8 put-ok?)]
+  (let [eq-maps (permutation 8 put-ok?)]
     (doseq [eq-map eq-maps] (show eq-map))
     (println "map count:" (count eq-maps))))
